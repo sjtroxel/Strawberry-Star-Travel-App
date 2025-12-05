482c9164eaf82d34885afa2fd5b4ada383b92f66
sjtroxel <sjtroxel@protonmail.com>
2025-12-05 16:08:20 -0600

    load seed data into StarsList.tsx, map over it and render data to page

--- strawberry-star-travel-app/src/features/stars/components/StarsList.tsx
+++ strawberry-star-travel-app/src/features/stars/components/StarsList.tsx
@@ -1,5 +1,16 @@
+import { stars } from "../data/stars";
+import type { Star } from "../types/Star";
+
 function StarsList() {
-  return <p>Loading stars...</p>;
+  return (
+    <ul>
+      {stars.map((star: Star) => (
+        <li key={star.id}>
+          {star.name} — {star.distanceLy.toFixed(2)} light-years away
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default StarsList;
