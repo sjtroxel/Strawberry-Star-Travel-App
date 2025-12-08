ba39e84b1e531c9124300bdd4dba9ee6d51d73e2
sjtroxel <sjtroxel@protonmail.com>
2025-12-08 13:40:46 -0600

    setup React Router and restructure app into multi-page layout

--- strawberry-star-travel-app/src/App.tsx
+++ strawberry-star-travel-app/src/App.tsx
@@ -1,11 +1,29 @@
+import { Routes, Route } from "react-router-dom";
 import StarsList from "./features/stars/components/StarsList";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
 
 function App() {
   return (
-    <main>
-      <h1>Strawberry Star Travel App</h1>
-      <StarsList />
-    </main>
+    <Routes>
+      {/* Home Page */}
+      <Route path="/" element={<h1>Welcome to the Strawberry Star Travel App</h1>} />
+
+      {/* Login & Signup */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Stars / Browse page */}
+      <Route 
+        path="/browse-stars" 
+        element={
+          <main>
+            <h1>Browse Stars</h1>
+            <StarsList />
+          </main>
+        } 
+      />
+    </Routes>
   );
 }
 
